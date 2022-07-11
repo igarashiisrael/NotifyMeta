1d06c9ad85d5b9bea14675c59cebea1f36799326
Israel <igarashiisrael@icloud.com>
2022-07-11 13:10:50 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+
+      <main>
+        <section id="sales">
+          <div className="notifymeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+function Header() {
+  return (
+    <>
+      <header>
+
+        <div className="notifyMeta-logo-container">
+          <img src={logo} alt="NotifyMeta" />
+          <h1>NotifyMeta</h1>
+          <p>Desenvolvido por
+            <a href="https://www.linkedin.com/in/israel-igarashi/">@Israel Igarashi</a>
+          </p>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
