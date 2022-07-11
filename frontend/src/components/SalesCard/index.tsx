75ea5bfa5e4c47f552cf27632e08fe5588597f3a
Israel <igarashiisrael@icloud.com>
2022-07-11 13:16:45 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,3 +1,5 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 
@@ -7,10 +9,20 @@ function SalesCard() {
       <h2 className="notifymeta-sales-title">Vendas</h2>
       <div>
         <div className="notifymeta-form-control-container">
-          <input className="notifymeta-form-control" type="text" />
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="notifymeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
         <div className="notifymeta-form-control-container">
-          <input className="notifymeta-form-control" type="text" />
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="notifymeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
       <div>
