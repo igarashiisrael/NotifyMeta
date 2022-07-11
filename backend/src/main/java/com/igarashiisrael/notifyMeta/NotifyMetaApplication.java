844ad549041205034cfc007f9ce3a9109a5f3bfe
Israel <igarashiisrael@icloud.com>
2022-07-11 11:36:06 -0300

    Project created

---
+++ backend/src/main/java/com/igarashiisrael/notifyMeta/NotifyMetaApplication.java
@@ -0,0 +1,13 @@
+package com.igarashiisrael.notifyMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyMetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/igarashiisrael/notifyMeta/NotifyMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igarashiisrael.notifyMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
