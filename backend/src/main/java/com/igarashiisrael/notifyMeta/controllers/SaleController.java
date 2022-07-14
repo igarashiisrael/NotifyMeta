5324a85f3dd6092557305006d44c4773f69267aa
Israel <igarashiisrael@icloud.com>
2022-07-13 21:40:31 -0300

    Twilio SMS

--- backend/src/main/java/com/igarashiisrael/notifyMeta/controllers/SaleController.java
+++ backend/src/main/java/com/igarashiisrael/notifyMeta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package com.igarashiisrael.notifyMeta.controllers;
 
 import com.igarashiisrael.notifyMeta.entities.Sale;
 import com.igarashiisrael.notifyMeta.services.SaleService;
+import com.igarashiisrael.notifyMeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,9 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
 
@@ -26,5 +27,10 @@ public class SaleController {
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
                                 Pageable pageable) {
         return service.findSales(minDate, maxDate,pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
